dab279759465678a289ac43fd8f0e2c72b243f5c
Jochen <gizzy@gmx.de>
2019-08-27 17:31:28 +0200

    updated prepare_update.sh

--- prepare_update.sh
+++ prepare_update.sh
@@ -2,10 +2,11 @@
 rm controls_grohe_ondus.txt
 while IFS= read -r -d '' FILE
 do
-    TIME=$(git log --pretty=format:%cd -n 1 --date=iso -- "$FILE")
-    TIME=$(TZ=Europe/Berlin date -d "$TIME" +%Y-%m-%d_%H:%M:%S)
-    FILESIZE=$(stat -c%s "$FILE")
+  TIME=$(git log --pretty=format:%cd -n 1 --date=iso -- "$FILE")
+  TIME=$(TZ=Europe/Berlin date -d "$TIME" +%Y-%m-%d_%H:%M:%S)
+  FILESIZE=$(stat -c%s "$FILE")
 	FILE=$(echo "$FILE"  | cut -c 3-)
+	#printf "DEL %s\n" "$FILE"  >> controls_grohe_ondus.txt
 	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls_grohe_ondus.txt
 done <   <(find ./FHEM -maxdepth 2 \( -name "*.pm" -o -name "*.txt" \) -print0 | sort -z -g)
 
